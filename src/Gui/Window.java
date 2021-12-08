2bfcb0df1c883a8886544e070543d666b469666f
ShauliTaragin <shaulitaragin@gmail.com>
2021-12-08 19:01:02 +0200

    added icons for gui menu and added center and shortest path algo

--- src/Gui/Window.java
+++ src/Gui/Window.java
@@ -3,15 +3,12 @@ package Gui;
 import api.EdgeData;
 import api.NodeData;
 import implementaions.*;
-import org.w3c.dom.Node;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.Line2D;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 
 import javax.swing.*;
 
@@ -35,7 +32,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
     private JMenu Help_menu;
     private JMenu editor;
     private JMenu Algorithms;
-    private JMenuItem load, save , show;
+    private JMenuItem load, save , clear;
     private JMenuItem AddNode;
     private JMenuItem RemoveNode;
     private JMenuItem Connect;
@@ -51,6 +48,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
     private double scale_lat;
     public ArrayList<NodeData> path;
     public Double dist;
+    public int nodeKey;
     public Window() {
         initGUI();
     }
@@ -61,27 +59,30 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         this.mWin_w= (int)(size.width/1.5);
         this.mWin_h= (int)(size.height/1.2);
-        System.out.println(mWin_w);
-        System.out.println(mWin_h);
         this.setSize(this.mWin_w,this.mWin_h);
         this.setVisible(true);
         this.menuBar = new JMenuBar();
         this.main_menu = new JMenu("Menu");
         this.Help_menu = new JMenu("Help");
+        this.Help_menu.setIcon(new ImageIcon("src\\Gui\\help.png"));
+        this.clear = new JMenuItem("Clear");
+        this.clear.addActionListener(this);
+        this.clear.setIcon(new ImageIcon("src\\Gui\\Eraser-icon.png"));
         this.menuBar.add(this.main_menu);
         this.menuBar.add(this.Help_menu);
+        this.menuBar.add(this.clear);
         this.setJMenuBar(this.menuBar);
         this.load = new JMenuItem("Load");
+        this.load.setIcon(new ImageIcon(("src\\Gui\\load.png")));
         this.load.addActionListener(this);
         this.main_menu.add(this.load);
         this.save = new JMenuItem("Save");
+        this.save.setIcon(new ImageIcon(("src\\Gui\\save.png")));
         this.save.addActionListener(this);
         this.main_menu.add(this.save);
-        this.show = new JMenuItem("Show");
-        this.show.addActionListener(this);
-        this.main_menu.add(this.show);
         this.editor = new JMenu("Edit Graph");
         this.editor.addActionListener(this);
+        this.editor.setIcon(new ImageIcon(("src\\Gui\\Pencil-icon.png")));
         this.main_menu.add(this.editor);
         this.AddNode = new JMenuItem("AddNode");
         this.AddNode.addActionListener(this);
@@ -98,6 +99,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
 
         this.Algorithms = new JMenu("Algorithms");
         this.Algorithms.addActionListener(this);
+        this.Algorithms.setIcon(new ImageIcon(("src\\Gui\\graph-icon.png")));
         this.main_menu.add(this.Algorithms);
 
         this.center = new JMenuItem("Center");
@@ -115,11 +117,16 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
         this.shortestDistPath = new JMenuItem("ShortestDistPath");
         this.shortestDistPath.addActionListener(this);
         this.Algorithms.add(this.shortestDistPath);
+        this.path=null;
+        this.dist=Double.MAX_VALUE;
+        this.nodeKey= Integer.MAX_VALUE;
 
         this.exit = new JMenuItem("Exit");
         this.exit.addActionListener(this);
+        this.exit.setIcon(new ImageIcon(("src\\Gui\\logout-icon.png")));
         this.main_menu.add(this.exit);
         this.main_menu.addSeparator();
+
         ButtonGroup group = new ButtonGroup();
         JRadioButtonMenuItem rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
         rbMenuItem.setSelected(true);
@@ -181,7 +188,15 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
 
             }
         }
+        if(this.nodeKey!=Integer.MAX_VALUE){
+            double x1 = (this.best_algo.getGraph().getNode(this.nodeKey).getLocation().x()-this.Minx)*this.scale_lon+60;
+            double y1 = (this.best_algo.getGraph().getNode(this.nodeKey).getLocation().y()-this.Miny)*this.scale_lat+60;
+            g.setColor(Color.CYAN);
+            g.fillOval((int) (x1-1.5*kRADIUS), (int) (y1-1.5*kRADIUS) , (int) (3 * kRADIUS), (int) (3 * kRADIUS));
+        }
         this.path=null;
+        this.dist=Double.MAX_VALUE;
+        this.nodeKey=Integer.MAX_VALUE;
     }
     private void drawArrowLine(Graphics2D g, int x1, int y1, int x2, int y2, int d, int h) {
         int dx = x2 - x1, dy = y2 - y1;
@@ -231,7 +246,8 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
         getContentPane().removeAll();
         container.setLayout(new FlowLayout());
         this.path=null;
-        this.dist=null;
+        this.dist=Double.MAX_VALUE;
+        this.nodeKey= Integer.MAX_VALUE;
         switch (e.getActionCommand()) {
             case ("Load"):
                 JFileChooser fileChooser = new JFileChooser();
@@ -249,8 +265,14 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
 //                fileChooser.setCurrentDirectory(new File("./data"));
 //                int response = fileChooser.showSaveDialog(null);
                 break;
-            case ("Show"):
-
+            case ("Clear"):
+                if (this.best_algo!=null) {
+                    show_graph(null, this.dist , this.nodeKey);
+                    this.setVisible(true);
+                }
+                break;
+            case ("Exit"):
+                dispose();
                 break;
             case ("AddNode"):
                 if (this.best_algo != null) {
@@ -258,9 +280,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
                     JTextField text_id = new JTextField();
                     JLabel label_geoloc = new JLabel("Enter geo location for Node:");
                     JTextField text_geoloc = new JTextField();
-
                     JButton submit_button = new JButton("Submit");
-
                     text_id.setPreferredSize(new Dimension(250, 40));
                     text_geoloc.setPreferredSize(new Dimension(250, 40));
                     submit_button.addActionListener(new ActionListener() {
@@ -271,11 +291,13 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
                                 int id = Integer.parseInt(text_id.getText());
                                 GeoL geoL = new GeoL(Double.parseDouble(geoloc[0]), Double.parseDouble(geoloc[1]), 0.0);
                                 best_algo.getGraph().addNode(new NodeD(id, geoL));
-                                show_graph(null,Double.MAX_VALUE);
+                                show_graph(null,Double.MAX_VALUE,Integer.MAX_VALUE);
                             }
                             catch (Exception exception){
-                                JLabel label_error = new JLabel("Error. Enter correct id and geolocation");
-                                container.add(label_error);
+                                JFrame f=new JFrame();
+                                JOptionPane.showMessageDialog(f,"Error, invalid arguments.\nEnter correct id and geo location");
+//                                JLabel label_error = new JLabel("Error. Enter correct id and geolocation");
+//                                container.add(label_error);
                                 setVisible(true);
                             }
                         }
@@ -285,6 +307,59 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
                     container.add(label_geoloc);
                     container.add(text_geoloc);
                     container.add(submit_button);
+                    this.setVisible(true);
+                    break;
+                }
+            case ("ShortestDist"):
+                if (this.best_algo != null) {
+                    JLabel label_src = new JLabel("Enter id for source Node");
+                    JTextField text_src = new JTextField();
+                    JLabel label_dest = new JLabel("Enter id for destination Node:");
+                    JTextField text_dest = new JTextField();
+                    JButton submit_button = new JButton("Submit");
+                    text_src.setPreferredSize(new Dimension(250, 40));
+                    text_dest.setPreferredSize(new Dimension(250, 40));
+                    submit_button.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e)  {
+                            JLabel label_ans=new JLabel();
+                            try{
+                                int dest = Integer.parseInt(text_dest.getText());
+                                int src = Integer.parseInt(text_src.getText());
+                                double dist=best_algo.shortestPathDist(src,dest);
+                                label_ans.setText("The shortest dist from "+" "+src+" "+"to "+" "+dest+" "+"is:"+dist);
+                                container.add(label_ans);
+                                setVisible(true);
+                            }
+                            catch (Exception exception){
+                                JFrame f=new JFrame();
+                                JOptionPane.showMessageDialog(f,"Error, no path between source and dest.\nEnter correct src and dest");
+//                                JLabel label_error = new JLabel("Error, no path between source and dest.\n Enter correct src and dest");
+//                                container.add(label_error);
+                                setVisible(true);
+                            }
+                        }
+                    });
+                    container.add(label_src);
+                    container.add(text_src);
+                    container.add(label_dest);
+                    container.add(text_dest);
+                    container.add(submit_button);
+                    this.setVisible(true);
+                    break;
+                }
+            case ("Center"):
+                if (this.best_algo != null) {
+                    try {
+                        this.nodeKey = this.best_algo.center().getKey();
+                        show_graph(null, Double.MAX_VALUE, this.nodeKey);
+                    } catch (Exception exception) {
+                        JFrame f = new JFrame();
+                        JOptionPane.showMessageDialog(f, "Error, invalid arguments.\nEnter correct id and geo location");
+//                                JLabel label_error = new JLabel("Error. Enter correct id and geolocation");
+//                                container.add(label_error);
+                        this.setVisible(true);
+                    }
                     this.setVisible(true);
                     break;
                 }
@@ -306,11 +381,13 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
                                 int dest = Integer.parseInt(text_dest.getText());
                                 int src = Integer.parseInt(text_src.getText());
                                 ArrayList<NodeData> temp=(ArrayList<NodeData>) best_algo.shortestPath(src,dest);
-                                show_graph(temp,Double.MAX_VALUE);
+                                show_graph(temp,Double.MAX_VALUE,Integer.MAX_VALUE);
                             }
                             catch (Exception exception){
-                                JLabel label_error = new JLabel("Error. Enter correct src and dest");
-                                container.add(label_error);
+                                JFrame f=new JFrame();
+                                JOptionPane.showMessageDialog(f,"Error, no path between source and dest.\nEnter correct src and dest");
+//                                JLabel label_error = new JLabel("Error, no path between source and dest.\n Enter correct src and dest");
+//                                container.add(label_error);
                                 setVisible(true);
                             }
                         }
@@ -343,7 +420,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
         this.scale_lon = ((this.mWin_w-100)/Absx);
         this.scale_lat = ((this.mWin_h-100)/Absy);
     }
-    public void show_graph(ArrayList<NodeData> path,double dist){
+    public void show_graph(ArrayList<NodeData> path,double dist,int nodeKey){
         mBuffer_image = createImage(mWin_w, mWin_h);
         mBuffer_graphics = mBuffer_image.getGraphics();
         this.getGraphics().drawImage(mBuffer_image, 0, 0, this);
@@ -352,6 +429,7 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
         container.setLayout(new FlowLayout());
         this.path=path;
         this.dist=dist;
+        this.nodeKey=nodeKey;
         repaint();
     }
     @Override
@@ -380,7 +458,6 @@ public class Window extends JFrame implements ActionListener, MouseListener, Mou
                 x_scale -=kRADIUS;
                 y_scale -=kRADIUS;
                 if ((Math.abs(x_scale- e.getX())<=kRADIUS) && (Math.abs(y_scale- e.getY())<=kRADIUS)){
-                    System.out.println("pressed");
                     drawgeo(getGraphics(), (GeoL) node.getNode().getLocation() , x_scale , y_scale);
                 }
             }
